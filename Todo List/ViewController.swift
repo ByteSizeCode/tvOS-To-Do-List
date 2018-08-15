75d720e018c97bbb6a155a3bd57740b4b8c1a7e4
Isaac Raval <Isaac@client.openvpn.net>
2018-08-14 20:45:31 -0700

    Working Prototype with one cell

--- Todo List/ViewController.swift
+++ Todo List/ViewController.swift
@@ -9,10 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //TEXT FIELD
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    //LIST BUTTONS
+    @IBOutlet weak var one: UIButton!
+    
+    @IBOutlet weak var two: UIButton!
+    
+    @IBOutlet weak var three: UIButton!
+    
+    @IBOutlet weak var four: UIButton!
+    
+    @IBOutlet weak var five: UIButton!
+    
+    @IBOutlet weak var six: UIButton!
+    
+    @IBOutlet weak var seven: UIButton!
+    
+    //    var buttons = ["button1", "button2", "button3", "button4", "button5", "button6", "button7"]
+    
+    @IBOutlet var collectionOfButtons: Array<UIButton>?
 
+    
+    //Add Item Button
+    @IBAction func addItemButton(_ sender: UIButton) {
+        
+        one.isHidden = false
+        
+        //Set button to on or off depending
+        var text: String = textField.text!
+        
+        one.setTitle(text, for: .normal)
+        
+        //Get inpiut fot title from UITextField
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        for item in collectionOfButtons! {
+//            
+//        }
     }
 
     override func didReceiveMemoryWarning() {
